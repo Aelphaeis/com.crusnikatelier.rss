dcf6eb498b393cdb66ac557740a08a4ec6cd2ce5
Joseph <aelphaeis@live.com>
2015-12-29 08:43:41 -0500

    Created Tests for Item pojo

---
+++ src/test/com/crusnikatelier/rss/pojos/ItemTest.java
@@ -0,0 +1,40 @@
+package test.com.crusnikatelier.rss.pojos;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import com.crusnikatelier.rss.exceptions.SyndicationSyntaxException;
+import com.crusnikatelier.rss.pojos.Item;
+
+public class ItemTest {
+
+	@Test
+	public void constructorSuccessTest() {
+		Item i = new Item();
+		assertNotNull(i);
+	}
+	
+	@Test(expected=SyndicationSyntaxException.class)
+	public void toElementInvalidTitleFailureTest(){
+		Item i = new Item();
+		i.toElement();
+	}
+	
+	@Test
+	public void toElementTitleSuccessTest(){
+		Item i = new Item();
+		i.setTitle("My Title");
+		i.toElement();
+		assertTrue(i.isValid());
+	}
+	
+	@Test
+	public void toElementDescriptionSucceessTest(){
+		Item i = new Item();
+		i.setDescription("My Description");
+		i.toElement();
+		assertTrue(i.isValid());
+	}
+	
+}
